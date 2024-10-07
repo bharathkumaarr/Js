0cd99634692510d35962365f6ef04e63aee67eb1
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2024-10-07 19:59:18 +0530

    update 10

--- assignments/scripts/10.js
+++ assignments/scripts/10.js
@@ -1,19 +1,25 @@
-const checkk = document.querySelector('.js-button')
-// console.log(checkk.classList.contains('js-button'));
-const gamingButtonElement = document.querySelector('.js-gaming-button');
-const musicElement = document.querySelector('.js-music-button');
-const techElement = document.querySelector('.js-tech-button');
+ const checkk = document.querySelector('.js-button')
+      // console.log(checkk.classList.contains('js-button'));
+      const gamingButtonElement = document.querySelector('.js-gaming-button');
+      const musicElement = document.querySelector('.js-music-button');
+      const techElement = document.querySelector('.js-tech-button');
+      
 
 
+      function toggle(para){
+        
+        if (!para.classList.contains('is-toggled')){
+          gamingButtonElement.classList.remove('is-toggled');
+          musicElement.classList.remove('is-toggled');
+          techElement.classList.remove('is-toggled');
 
-function toggle(para){
-  
-  if (para.classList.contains('is-toggled') || para.classList.contains('is-toggled') || para.classList.contains('is-toggled')){
-    para.classList.remove('is-toggled');
-  } 
-  else {
-    para.classList.add('is-toggled');
-  }
-}
+          para.classList.add('is-toggled');
+        } 
+        else {
+          para.classList.remove('is-toggled');
 
+          
+        }
+      }
+      
 
